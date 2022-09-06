c88b757f29214cf20ab76722b20bb32b741464ad
islammd39e <islammdrahat308@gmail.comm>
2022-09-06 17:54:09 +0600

    input file

---
+++ interact.js
@@ -0,0 +1,5 @@
+document.getElementById('add').addEventListener('click', function(){
+    const field = document.getElementById('input-field1');
+    const value = field.value;
+    console.log(value);
+})
